af1bd91e7f0844f44f28a03255bed584da29fc22
manisha-13-code <manishachauhan5469@gmail.com>
2025-12-27 00:17:36 +0530

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -59,12 +59,28 @@ const services = [
 
 const technologies = [
   {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
     name: "JavaScript",
     icon: javascript,
   },
   {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
     name: "React JS",
     icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
   },
   {
     name: "Tailwind CSS",
